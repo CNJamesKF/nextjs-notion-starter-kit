1870a7693b0d7f159ea4dd4662d74e47d60303ef
JamesKF <44717201+CNJamesKF@users.noreply.github.com>
2023-03-01 16:11:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'ad3cab034495403a9235c10507ee2f4b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'BigBigBear的个人博客',
+  domain: 'https://bigbigbear.notion.site/bigbigbear/bigbigbear-ad3cab034495403a9235c10507ee2f4b',
+  author: 'BigBigBear',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
