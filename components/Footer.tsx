90f786d509ec42fd7213088bf3b1c38effcebfcf
JamesKF <44717201+CNJamesKF@users.noreply.github.com>
2023-03-17 13:39:11 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+
       <div className={styles.Beian}> 
         <a href="https://beian.miit.gov.cn/">宁ICP备20000276号</a>  
       </div>
